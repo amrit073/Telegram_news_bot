4c82cc840d43a169d3ba5063b961dfdeead3ed8c
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2022-04-16 19:17:43 +0545

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def NewsFromBBC():
     query_params = {
       "source": "bbc-news",
       "sortBy": "top",
-      "apiKey": "4dbc17e007ab436fb66416009dfb59a8"
+      "apiKey": "haha"
     }
     main_url = " https://newsapi.org/v1/articles"
  
